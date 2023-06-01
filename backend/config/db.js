29f27fe60c5853e54edae1ec8ce9e6894362d8cd
Luiz <luiz7tenshi@gmail.com>
2023-05-31 22:06:51 -0300

    backend

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+const conn = async () => {
+    try{
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.9ewoxoi.mongodb.net/?retryWrites=true&w=majority`)
+        console.log('Conectou ao banco')
+        return dbConn
+    } catch (error) {
+        console.log(error)
+     }
+}
+
+conn()
+
+module.exports = conn
+//conection
